50ca93266aabafaebf971803fb654e6bd1cec756
anshuman115 <anshuman.tripathy@aurm.in>
2026-06-07 20:34:33 +0530

    chore: repurpose repo as Go server, remove Flutter scaffold, add CLAUDE.md

--- android/app/src/main/kotlin/com/heliolytics/heliolytics/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.heliolytics.heliolytics
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/auth/auth_key_store.dart
+++
@@ -1,5 +0,0 @@
-abstract class AuthKeyStore {
-  Future<void> write(String key, String value);
-  Future<String?> read(String key);
-  Future<void> delete(String key);
-}
